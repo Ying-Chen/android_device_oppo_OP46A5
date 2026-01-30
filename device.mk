365279d3abdb33a3ba6abe74a330cdee1c46190b
Ivan Vecera <ivan@cera.cz>
2026-01-30 21:09:56 +0800

    PBEM00: Build Lineage Health HAL

--- device.mk
+++ device.mk
@@ -177,6 +177,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
