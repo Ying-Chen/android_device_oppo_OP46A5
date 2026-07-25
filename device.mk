2af3a31fd1f462713ca305d92b05fe707302dbfe
Ying-Chen <2132703527@qq.com>
2026-07-25 17:54:21 +0800

    PBEM00: Removed unused partition odm

--- device.mk
+++ device.mk
@@ -308,8 +308,7 @@ PRODUCT_COPY_FILES += \
 
 # Rootdir
 PRODUCT_PACKAGES += \
-    fstab.qcom \
-    fstab.qcom_ramdisk
+    fstab.qcom
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -52,7 +52,7 @@ LOCAL_MODULE       := init.oppo.fingerprint.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.oppo.fingerprint.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM_ETC)/init/
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -95,13 +95,4 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.qcom_ramdisk
-LOCAL_MODULE_STEM  := fstab.qcom
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.qcom
-LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
