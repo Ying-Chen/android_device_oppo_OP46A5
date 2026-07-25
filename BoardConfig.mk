2de84cab74772ef7027f2c5674342182297ed221
Ying-Chen <2132703527@qq.com>
2026-07-25 18:24:52 +0800

    Revert "OP46B1: Drop unused BOARD_ROOT_EXTRA_SYMLINKS"
    This reverts commit 9be0f82ce91ab4e140a17bba6e36ada62cff3e80.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,11 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 # Platform
 TARGET_BOARD_PLATFORM := sdm710
 
+BOARD_ROOT_EXTRA_SYMLINKS := \
+    /vendor/firmware_mnt:/firmware \
+    /vendor/bt_firmware:/bt_firmware \
+    /vendor/dsp:/dsp
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
@@ -147,6 +152,7 @@ VENDOR_SECURITY_PATCH := 2022-04-05
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
 BOARD_VNDK_VERSION := current
